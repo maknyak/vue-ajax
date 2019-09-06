40807d4882e7f1123ca47bc40dda121149ef3474
Ian Maulana <ian_maulana@yahoo.com>
2019-09-06 17:23:27 +0700

    implement vee validate

--- src/views/pages/LoginPage.vue
+++ src/views/pages/LoginPage.vue
@@ -8,17 +8,17 @@
             <h1 class="title-login">EACT</h1>
             <p class="tagline-login"><strong>Selamat datang!</strong> Silakan masuk <br/> menggunakan Username / Email yang terdaftar.</p>
 
-            <form action="">
+            <form novalidate @submit.prevent="login">
               <div class="form-group">
-                <input type="text" class="form-control" placeholder="Username / Email">
+                <input type="text" name="username" class="form-control" placeholder="Username / Email" v-model="username" v-validate="'required'">
+                <div class="invalid-feedback">{{ errors.first('username') }}</div>
               </div>
               <div class="form-group">
-                <input type="text" class="form-control" placeholder="Kata sandi">
-              </div>
-              <div class="form-group">
-                <a href="">Lupa kata sandi?</a>
+                <input type="password" name="password" class="form-control" placeholder="Kata sandi" v-model="password" v-validate="'required'">
+                <div class="invalid-feedback">{{ errors.first('password') }}</div>
               </div>
               <button type="submit" class="btn btn-primary">Masuk</button>
+              <a class="btn btn-outline-primary mx-2" href="#">Lupa kata sandi</a>
             </form>
           </div>
         </div>
@@ -34,7 +34,18 @@ export default {
   data: function () {
     return {
       loginbg: loginbg,
-      iconbg: iconbg
+      iconbg: iconbg,
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          this.$router.push('/')
+        }
+      })
     }
   }
 }
