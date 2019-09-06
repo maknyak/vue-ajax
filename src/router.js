5f841c401aeb6441cc360c6c388b4f5460e3ddde
Ian Maulana <ian_maulana@yahoo.com>
2019-09-06 12:38:59 +0700

    add dummy page to router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import AdminLayout from './views/layouts/AdminLayout'
 import LoginPage from './views/pages/LoginPage'
 import DashboardPage from './views/pages/DashboardPage'
+import PenggunaPage from './views/pages/PenggunaPage'
+import BlankPage from './views/pages/BlankPage'
 
 Vue.use(Router)
 
@@ -19,6 +21,16 @@ export default new Router({
           path: '/dashboard',
           name: 'dashboardPage',
           component: DashboardPage
+        },
+        {
+          path: '/pengguna',
+          name: 'penggunaPage',
+          component: PenggunaPage
+        },
+        {
+          path: '/blank',
+          name: 'blankPage',
+          component: BlankPage
         }
       ]
     },
